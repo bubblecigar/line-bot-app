6b72efe2174d3f6076f12d700f1ddce2ea95abb0
王瑞瑜 <bubblecigar@gmail.com>
2025-04-20 16:53:47 +0800

    fix app type as Express

--- src/routes/lineRoutes.ts
+++ src/routes/lineRoutes.ts
@@ -1,15 +1,10 @@
-import { Router } from 'express';
+import { Router, Express } from 'express';
 import { LineController } from '../controllers/lineController';
 
 const router = Router();
 const lineController = new LineController();
 
-// TODO: fix the app type
-type appType = {
-    use: (arg0: string, arg1: Router) => void;
-}
-
-export function setRoutes(app: appType) {
+export function setRoutes(app: Express) {
     app.use('/webhook', router);
     router.post('/', lineController.handleWebhook.bind(lineController));
 }
